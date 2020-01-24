e04b778ac5b4ef98a02ec0391a3582044e71cb8b
Richard Long <richard.long@momentumtelecom.com>
2020-01-24 16:55:31 -0500

    added models

--- device/admin.py
+++ device/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import DeviceType
+from . models import Device
+
+admin.site.register(DeviceType)
+admin.site.register(Device)
--- device/models.py
+++ device/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class DeviceType(model.Model):
+    type = models.CharField(max_length=250)
+    name = models.CharField(max_length=500)
+    class Meta:
+        verbose_name = "DeviceType"
+        verbose_name_plural = "DeviceTypes"
+        db_table = 'DeviceType'
+
+    def __str__(self):
+        return str(self.type)
+
+class Device(model.Model):
+    device_type_id = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
+    rack_unit_size = models.PositiveSmallIntegerField()
+    starting_rack_unit_position = Model.PositiveSmallIntegerField()
+    snding_rack_unit_position = Model.PositiveSmallIntegerField()
+    rack_id = Model.PositiveIntegerField()
+    class Meta:
+        verbose_name = "Device"
+        verbose_name_plural = "Devices"
+        db_table = 'Device'
+
+    def __str__(self):
+        return str(self.device_type_id)
